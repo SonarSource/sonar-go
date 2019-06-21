994df3c52e1a81e762c27c0e65a15bd340af3eeb
Michael Gumowski <michael.gumowski@sonarsource.com>
2019-06-21 08:43:12 +0200

    SONARSLANG-375 Setup plugin backbone for Slang support of Go (#42)

---
+++ sonar-go-plugin/src/main/java/org/sonar/go/plugin/GoProfileDefinition.java
@@ -0,0 +1,34 @@
+/*
+ * SonarQube Go Plugin
+ * Copyright (C) 2018-2019 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.go.plugin;
+
+import org.sonar.api.server.profile.BuiltInQualityProfilesDefinition;
+import org.sonarsource.analyzer.commons.BuiltInQualityProfileJsonLoader;
+
+public class GoProfileDefinition implements BuiltInQualityProfilesDefinition {
+
+  @Override
+  public void define(Context context) {
+    NewBuiltInQualityProfile profile = context.createBuiltInQualityProfile("Sonar way", GoLanguage.KEY);
+    BuiltInQualityProfileJsonLoader.load(profile, GoRulesDefinition.REPOSITORY_KEY, "org/sonar/l10n/go/rules/go/Sonar_way_profile.json");
+    profile.setDefault(true);
+    profile.done();
+  }
+}
