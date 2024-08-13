df9252c7ac794271b697eeb46b0f7012cc324cf6
Angelo Buono <angelo.buono@sonarsource.com>
2024-08-13 18:02:15 +0200

    SONARGO-1  Extract and build Go plugin (#1)

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
-# sonar-go
+# Sonar-Go
 
-Go Analyzer
+[![Build Status](https://api.cirrus-ci.com/github/SonarSource/slang.svg?branch=master)](https://cirrus-ci.com/github/SonarSource/slang)
+[![Quality Gate](https://sonarcloud.io/api/project_badges/measure?project=org.sonarsource.slang%3Aslang&metric=alert_status)](https://sonarcloud.io/dashboard?id=org.sonarsource.slang%3Aslang)
+[![Coverage](https://sonarcloud.io/api/project_badges/measure?project=org.sonarsource.slang%3Aslang&metric=coverage)](https://sonarcloud.io/component_measures/domain/Coverage?id=org.sonarsource.slang%3Aslang)
+
+This is a developer documentation. If you want to analyze source code in SonarQube read one of the following documentation:
+
+We use the native Go parser to parse the Go language.
+
+## Have questions or feedback?
+
+To provide feedback (request a feature, report a bug, etc.) use the [SonarQube Community Forum](https://community.sonarsource.com/). Please do not forget to specify the language, plugin version, and SonarQube version.
+
+## Building
+
+### Setup
+
+If you are on Windows, read the [sonar-go-to-slang/README.md](sonar-go-to-slang/README.md) instructions.
+
+
+### Build
+Build and run Unit Tests:
+
+    ./gradlew build
+
+## Integration Tests
+
+By default, Integration Tests (ITs) are skipped during builds.
+If you want to run them, you need first to retrieve the related projects which are used as input:
+
+    git submodule update --init its/sources
+
+Then build and run the Integration Tests using the `its` property:
+
+    ./gradlew build -Pits --info --no-daemon
+
+You can also build and run only Ruling Tests using the `ruling` property:
+
+    ./gradlew build -Pruling --info --no-daemon
+
+## License headers
+
+License headers are automatically updated by the spotless plugin but only for Java files. 
+Furthermore, there are files such as `package-info.java` and `module-info.java` that spotless ignores. 
+Also the Go source files are not handled. For those files use a manual script like below to update the license:
+    `find . -type f -name "*.go" -exec sed -i '' 's/2018-2023/2018-2024/' "{}" \;`
---
+++ its/plugin/src/test/java/org/sonarsource/slang/CoverageTest.java
@@ -0,0 +1,54 @@
+/*
+ * SonarSource Go
+ * Copyright (C) 2018-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.slang;
+
+import com.sonar.orchestrator.build.SonarScanner;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CoverageTest extends TestBase {
+
+  private static final Path BASE_DIRECTORY = Paths.get("projects", "measures");
+
+  @Rule
+  public TemporaryFolder tmpDir = new TemporaryFolder();
+
+  @Test
+  public void go_coverage() {
+    final String projectKey = "goCoverage";
+    SonarScanner goScanner = getSonarScanner(projectKey, BASE_DIRECTORY.toString(), "go");
+    goScanner.setProperty("sonar.go.coverage.reportPaths", "coverage.out");
+
+    ORCHESTRATOR.executeBuild(goScanner);
+
+    String componentKey = projectKey + ":pivot.go";
+    assertThat(getMeasureAsInt(componentKey, "lines_to_cover")).isEqualTo(16);
+    assertThat(getMeasureAsInt(componentKey, "uncovered_lines")).isEqualTo(4);
+    assertThat(getMeasureAsInt(componentKey, "conditions_to_cover")).isNull();
+    assertThat(getMeasureAsInt(componentKey, "uncovered_conditions")).isNull();
+  }
+}
