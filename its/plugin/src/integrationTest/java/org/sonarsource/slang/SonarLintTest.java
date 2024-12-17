1428844f61276dad4f20d0cff3e37ff21e469b90
Marcin Stachniuk <marcin.stachniuk@sonarsource.com>
2024-12-17 13:04:30 +0100

    SONARGO-80 Update sonarlint-core dependency

--- its/plugin/src/integrationTest/java/org/sonarsource/slang/SonarLintTest.java
+++ its/plugin/src/integrationTest/java/org/sonarsource/slang/SonarLintTest.java
@@ -16,165 +16,153 @@
  */
 package org.sonarsource.slang;
 
-import com.sonar.orchestrator.Orchestrator;
-import com.sonar.orchestrator.locator.Locators;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.nio.charset.Charset;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
+import java.time.Duration;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 import org.apache.commons.io.FileUtils;
-import org.assertj.core.groups.Tuple;
-import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.api.AfterAll;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
-import org.sonarsource.sonarlint.core.StandaloneSonarLintEngineImpl;
-import org.sonarsource.sonarlint.core.analysis.api.ClientInputFile;
-import org.sonarsource.sonarlint.core.client.api.common.analysis.Issue;
-import org.sonarsource.sonarlint.core.client.api.standalone.StandaloneAnalysisConfiguration;
-import org.sonarsource.sonarlint.core.client.api.standalone.StandaloneGlobalConfiguration;
-import org.sonarsource.sonarlint.core.client.api.standalone.StandaloneSonarLintEngine;
-import org.sonarsource.sonarlint.core.commons.IssueSeverity;
-import org.sonarsource.sonarlint.core.commons.Language;
+import org.sonarsource.sonarlint.core.rpc.client.ClientJsonRpcLauncher;
+import org.sonarsource.sonarlint.core.rpc.client.SonarLintRpcClientDelegate;
+import org.sonarsource.sonarlint.core.rpc.impl.BackendJsonRpcLauncher;
+import org.sonarsource.sonarlint.core.rpc.protocol.SonarLintRpcServer;
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.analysis.AnalyzeFilesParams;
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.analysis.AnalyzeFilesResponse;
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.file.DidUpdateFileSystemParams;
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.initialize.ClientConstantInfoDto;
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.initialize.FeatureFlagsDto;
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.initialize.HttpConfigurationDto;
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.initialize.InitializeParams;
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.initialize.TelemetryClientConstantAttributesDto;
+import org.sonarsource.sonarlint.core.rpc.protocol.common.ClientFileDto;
+import org.sonarsource.sonarlint.core.rpc.protocol.common.IssueSeverity;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.tuple;
+import static org.sonarsource.slang.TestBase.GO_PLUGIN_LOCATION;
 
 public class SonarLintTest {
 
-  @TempDir
-  static Path baseDir;
+  private static final ClientConstantInfoDto IT_CLIENT_INFO = new ClientConstantInfoDto("clientName", "integrationTests");
+  private static final TelemetryClientConstantAttributesDto IT_TELEMETRY_ATTRIBUTES = new TelemetryClientConstantAttributesDto(
+    "SonarLint ITs", "SonarLint ITs", "1.2.3", "4.5.6", Collections.emptyMap());
+  private static final String CONFIG_SCOPE_ID = "my-ide-project-name";
 
-  private static StandaloneSonarLintEngine sonarlintEngine;
+  private static final boolean SHOULD_MANAGE_SMART_NOTIFICATIONS = true;
+  private static final boolean TAINT_VULNERABILITIES_ENABLED = true;
+  private static final boolean SHOULD_SYNCHRONIZE_PROJECTS = true;
+  private static final boolean SHOULD_NOT_MANAGE_LOCAL_SERVER = false;
+  private static final boolean ENABLE_SECURITY_HOTSPOTS = true;
+  private static final boolean SHOULD_MANAGE_SERVER_SENT_EVENTS = true;
+  private static final boolean DISABLE_DATAFLOW_BUG_DETECTION = false;
+  private static final boolean SHOULD_MANAGE_FULL_SYNCHRONIZATION = true;
+  private static final boolean DISABLE_TELEMETRY = false;
+  private static final boolean CAN_NOT_OPEN_FIX_SUGGESTION = false;
+
+  @TempDir
+  static Path sonarUserHome;
+
+  private static SonarLintRpcServer backend;
+  private static SonarLintRpcClientDelegate client;
 
   @BeforeAll
-  public static void prepare() {
-    Orchestrator orchestrator = TestBase.ORCHESTRATOR;
+  public static void prepare() throws Exception {
+    var clientToServerOutputStream = new PipedOutputStream();
+    var clientToServerInputStream = new PipedInputStream(clientToServerOutputStream);
 
-    Locators locators = orchestrator.getConfiguration().locators();
-    StandaloneGlobalConfiguration.Builder sonarLintConfigBuilder = StandaloneGlobalConfiguration.builder();
-    orchestrator.getDistribution().getPluginLocations().stream()
-      .filter(location -> !location.toString().contains("sonar-reset-data-plugin"))
-      .map(plugin -> locators.locate(plugin).toPath())
-      .forEach(sonarLintConfigBuilder::addPlugin);
+    var serverToClientOutputStream = new PipedOutputStream();
+    var serverToClientInputStream = new PipedInputStream(serverToClientOutputStream);
+    client = new MockSonarLintRpcClientDelegate();
+    new BackendJsonRpcLauncher(clientToServerInputStream, serverToClientOutputStream);
+    var clientLauncher = new ClientJsonRpcLauncher(serverToClientInputStream, clientToServerOutputStream, client);
+    backend = clientLauncher.getServerProxy();
 
-    sonarLintConfigBuilder
-      .setSonarLintUserHome(baseDir)
-      .setLogOutput((formattedMessage, level) -> {
-        /* Don't pollute logs */
-      });
-    StandaloneGlobalConfiguration configuration = sonarLintConfigBuilder
-      .addEnabledLanguage(Language.GO)
-      .build();
-    sonarlintEngine = new StandaloneSonarLintEngineImpl(configuration);
+    var featureFlags = new FeatureFlagsDto(
+      SHOULD_MANAGE_SMART_NOTIFICATIONS,
+      TAINT_VULNERABILITIES_ENABLED,
+      SHOULD_SYNCHRONIZE_PROJECTS,
+      SHOULD_NOT_MANAGE_LOCAL_SERVER,
+      ENABLE_SECURITY_HOTSPOTS,
+      SHOULD_MANAGE_SERVER_SENT_EVENTS,
+      DISABLE_DATAFLOW_BUG_DETECTION,
+      SHOULD_MANAGE_FULL_SYNCHRONIZATION,
+      DISABLE_TELEMETRY,
+      CAN_NOT_OPEN_FIX_SUGGESTION);
+    backend.initialize(
+        new InitializeParams(IT_CLIENT_INFO, IT_TELEMETRY_ATTRIBUTES, HttpConfigurationDto.defaultConfig(), null, featureFlags,
+          sonarUserHome.resolve("storage"),
+          sonarUserHome.resolve("work"),
+          Set.of(GO_PLUGIN_LOCATION.getFile().toPath()), Collections.emptyMap(),
+          Set.of(org.sonarsource.sonarlint.core.rpc.protocol.common.Language.GO), Collections.emptySet(), Collections.emptySet(), Collections.emptyList(), Collections.emptyList(), sonarUserHome.toString(), Map.of(),
+          false, null, false, null))
+      .get();
   }
 
-  @AfterAll
-  public static void stop() {
-    sonarlintEngine.stop();
+  @AfterEach
+  public void reset() {
+    ((MockSonarLintRpcClientDelegate) client).getRaisedIssues().clear();
   }
 
   @Test
   public void test_go() throws Exception {
-    ClientInputFile inputFile = prepareInputFile(
-      "package main\n"
-        + "func empty() {\n" // go:S1186 (empty function)
-        + "}\n");
+    var inputFile = prepareInputFile("""
+      package main
+      func empty() {
+      }
+      """);
 
-    assertIssues(analyzeWithSonarLint(inputFile),
-      tuple("go:S1186", 2, inputFile.getPath(), IssueSeverity.CRITICAL));
+    var analyzeResponse = analyzeFile(inputFile);
+    assertThat(analyzeResponse.getFailedAnalysisFiles()).isEmpty();
+    Awaitility.await().atMost(Duration.ofMillis(200)).untilAsserted(() -> assertThat(((MockSonarLintRpcClientDelegate) client).getRaisedIssues(CONFIG_SCOPE_ID)).isNotEmpty());
+    var raisedIssues = ((MockSonarLintRpcClientDelegate) client).getRaisedIssues(CONFIG_SCOPE_ID);
+    assertThat(raisedIssues).hasSize(1);
+    var issue = raisedIssues.get(0);
+    assertThat(issue.getRuleKey()).isEqualTo("go:S1186");
+    assertThat(issue.getTextRange().getStartLine()).isEqualTo(2);
+    assertThat(issue.getFileUri().getPath()).isEqualTo(inputFile.toString());
+    assertThat(issue.getSeverity()).isEqualTo(IssueSeverity.CRITICAL);
   }
 
   @Test
   public void test_go_nosonar() throws Exception {
-    ClientInputFile goInputFile = prepareInputFile(
-      "package main\n"
-        + "func empty() { // NOSONAR\n" // skipped go:S1186 (empty function)
-        + "}\n");
-    assertThat(analyzeWithSonarLint(goInputFile)).isEmpty();
+    var inputFile = prepareInputFile("""
+      package main
+      func empty() { // NOSONAR
+      }
+      """);
+
+    var analyzeResponse = analyzeFile(inputFile);
+    assertThat(analyzeResponse.getFailedAnalysisFiles()).isEmpty();
+    Awaitility.await().atMost(Duration.ofMillis(200));
+    var raisedIssues = ((MockSonarLintRpcClientDelegate) client).getRaisedIssues(CONFIG_SCOPE_ID);
+    assertThat(raisedIssues).hasSize(0);
   }
 
-  private List<Issue> analyzeWithSonarLint(ClientInputFile inputFile) {
-    List<Issue> issues = new ArrayList<>();
-    StandaloneAnalysisConfiguration analysisConfiguration = StandaloneAnalysisConfiguration.builder()
-      .setBaseDir(baseDir)
-      .addInputFiles(Collections.singletonList(inputFile))
-      .build();
+  private AnalyzeFilesResponse analyzeFile(Path filePath) {
+    var fileUri = filePath.toUri();
+    backend.getFileService().didUpdateFileSystem(new DidUpdateFileSystemParams(List.of(),
+      List.of(new ClientFileDto(fileUri, filePath, CONFIG_SCOPE_ID, false, null, filePath.toAbsolutePath(), null, null, true)),
+      List.of()));
 
-    sonarlintEngine.analyze(analysisConfiguration, issues::add, null, null);
-
-    return issues;
+    return backend.getAnalysisService().analyzeFiles(
+      new AnalyzeFilesParams(CONFIG_SCOPE_ID, UUID.randomUUID(), List.of(fileUri), new HashMap<>(), System.currentTimeMillis())).join();
   }
 
-  private void assertIssues(List<Issue> issues, Tuple... expectedIssues) {
-    assertThat(issues)
-      .extracting(Issue::getRuleKey, Issue::getStartLine, issue -> issue.getInputFile().getPath(), Issue::getSeverity)
-      .containsExactlyInAnyOrder(expectedIssues);
-  }
-
-  private ClientInputFile prepareInputFile(String content) throws IOException {
-    File file = new File(baseDir.toFile(), "foo.go");
+  private Path prepareInputFile(String content) throws IOException {
+    File file = new File(sonarUserHome.toFile(), "foo.go");
     FileUtils.write(file, content, StandardCharsets.UTF_8);
-    return createInputFile(file.toPath());
+    return file.toPath();
   }
-
-  private ClientInputFile createInputFile(final Path path) {
-    return new ClientInputFile() {
-
-      @Override
-      public URI uri() {
-        return path.toUri();
-      }
-
-      @Override
-      public String getPath() {
-        return path.toString();
-      }
-
-      @Override
-      public boolean isTest() {
-        return false;
-      }
-
-      @Override
-      public Charset getCharset() {
-        return StandardCharsets.UTF_8;
-      }
-
-      @Override
-      public <G> G getClientObject() {
-        return null;
-      }
-
-      @Override
-      public String contents() throws IOException {
-        return Files.readString(path);
-      }
-
-      @Override
-      public String relativePath() {
-        return path.toString();
-      }
-
-      @Override
-      public InputStream inputStream() throws IOException {
-        return Files.newInputStream(path);
-      }
-
-      @NotNull
-      @Override
-      public Language language() {
-        return Language.forKey("go").get();
-      }
-    };
-  }
-
 }
