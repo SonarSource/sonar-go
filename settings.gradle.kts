0b950b4f7fef6cba1bf079b339d425ef651e0552
Peter Trifanov <peter.trifanov@sonarsource.com>
2024-12-17 10:44:18 +0100

    SONARGO-83 Create a settings plugin to set project version (#46)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,42 +1,9 @@
 pluginManagement {
     includeBuild("build-logic")
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-    }
 }
 
 plugins {
-    id("com.gradle.develocity") version "3.18.2"
-    id("com.diffplug.blowdryerSetup") version "1.7.1"
-}
-
-develocity {
-    server = "https://develocity.sonar.build"
-    buildScan {
-        tag(if (System.getenv("CI").isNullOrEmpty()) "local" else "CI")
-        tag(System.getProperty("os.name"))
-        if (System.getenv("CIRRUS_BRANCH") == "master") {
-            tag("master")
-        }
-        if (System.getenv("CIRRUS_PR")?.isBlank() == false) {
-            tag("PR")
-        }
-        value("Build Number", System.getenv("BUILD_NUMBER"))
-        value("Branch", System.getenv("CIRRUS_BRANCH"))
-        value("PR", System.getenv("CIRRUS_PR"))
-    }
-}
-
-val isCI = System.getenv("CI") != null
-buildCache {
-    local {
-        isEnabled = !isCI
-    }
-    remote(develocity.buildCache) {
-        isEnabled = true
-        isPush = isCI
-    }
+    id("org.sonarsource.cloud-native.common-settings")
 }
 
 rootProject.name = "sonar-go"
