e96eb8936f7ba24feef5c2420aa2ba298dc860d0
Jonas Wielage <jonas.wielage@sonarsource.com>
2025-01-30 10:32:53 +0100

    SONARGO-181 S4507: Implementation for `net/http/pprof`

--- sonar-go-frontend/src/main/java/org/sonar/go/utils/TreeUtils.java
+++ sonar-go-frontend/src/main/java/org/sonar/go/utils/TreeUtils.java
@@ -61,13 +61,7 @@ public class TreeUtils {
       .flatMap(it -> it.children().stream())
       .filter(StringLiteralTree.class::isInstance)
       .map(StringLiteralTree.class::cast)
-      .map(StringLiteralTree::value)
-      .map(TreeUtils::trimQuotes)
+      .map(StringLiteralTree::content)
       .collect(Collectors.toSet());
-  }
-
-  private static String trimQuotes(String text) {
-    // the imports are always surrounded by double-quotes
-    return text.substring(1, text.length() - 1);
   }
 }
