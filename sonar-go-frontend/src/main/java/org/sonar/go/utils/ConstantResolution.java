d917c18b0fdfc5af18132d9bdc7b56ed83cee658
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2025-02-25 10:47:47 +0100

    SONARGO-315 S5443 Advanced usages with variable resolution

--- sonar-go-frontend/src/main/java/org/sonar/go/utils/ConstantResolution.java
+++ sonar-go-frontend/src/main/java/org/sonar/go/utils/ConstantResolution.java
@@ -16,10 +16,15 @@
  */
 package org.sonar.go.utils;
 
+import java.util.Optional;
+import javax.annotation.Nullable;
 import org.sonar.go.api.BinaryExpressionTree;
+import org.sonar.go.api.IdentifierTree;
 import org.sonar.go.api.ParenthesizedExpressionTree;
 import org.sonar.go.api.StringLiteralTree;
 import org.sonar.go.api.Tree;
+import org.sonar.go.symbols.Symbol;
+import org.sonar.go.symbols.Usage;
 
 public class ConstantResolution {
 
@@ -33,15 +38,58 @@ public class ConstantResolution {
    * Try to resolve the tree as a String constant.
    * For any tree that cannot be resolved to a constant, a placeholder "_?_" will be introduced instead.
    */
-  public static String resolveAsStringConstant(Tree tree) {
+  public static String resolveAsStringConstant(@Nullable Tree tree) {
+    return resolveAsStringConstant(tree, true);
+  }
+
+  /**
+   * In order to avoid any risk of infinite recursion, we only follow identifier value once, depending on "followIdentifier" argument.
+   * This approximation seems reasonable because we want to support the obvious cases (global constant, local variable used as constant),
+   * and not trying to build a complex constant folding logic.
+   */
+  private static String resolveAsStringConstant(@Nullable Tree tree, boolean followIdentifier) {
+    if (tree == null) {
+      return PLACEHOLDER;
+    }
     if (tree instanceof StringLiteralTree stringLiteral) {
       return stringLiteral.content();
     } else if (tree instanceof BinaryExpressionTree binaryExpressionTree && binaryExpressionTree.operator() == BinaryExpressionTree.Operator.PLUS) {
-      return resolveAsStringConstant(binaryExpressionTree.leftOperand()) + resolveAsStringConstant(binaryExpressionTree.rightOperand());
+      return resolveAsStringConstant(binaryExpressionTree.leftOperand(), followIdentifier) + resolveAsStringConstant(binaryExpressionTree.rightOperand(), followIdentifier);
     } else if (tree instanceof ParenthesizedExpressionTree parenthesizedExpression) {
-      return resolveAsStringConstant(parenthesizedExpression.expression());
+      return resolveAsStringConstant(parenthesizedExpression.expression(), followIdentifier);
+    } else if (tree instanceof IdentifierTree identifier && followIdentifier) {
+      return resolveIdentifierAsStringConstant(identifier);
     }
     return PLACEHOLDER;
   }
 
+  private static String resolveIdentifierAsStringConstant(IdentifierTree identifier) {
+    Symbol symbol = identifier.symbol();
+    if (symbol == null) {
+      return PLACEHOLDER;
+    }
+    return getEffectivelyFinalUsage(symbol)
+      .map(Usage::value)
+      .map(v -> resolveAsStringConstant(v, false))
+      .orElse(PLACEHOLDER);
+  }
+
+  /**
+   * An identifier is effectively final if it is never reassigned.
+   */
+  public static Optional<Usage> getEffectivelyFinalUsage(Symbol symbol) {
+    Usage effectivelyFinalUsage = null;
+    for (Usage usage : symbol.getUsages()) {
+      if (usage.type() == Usage.UsageType.DECLARATION) {
+        // An identifier with multiple declarations should never happen, but if it ever does, we don't consider it as effectively final.
+        if (effectivelyFinalUsage != null) {
+          return Optional.empty();
+        }
+        effectivelyFinalUsage = usage;
+      } else if (usage.type() == Usage.UsageType.ASSIGNMENT) {
+        return Optional.empty();
+      }
+    }
+    return Optional.ofNullable(effectivelyFinalUsage);
+  }
 }
--- sonar-go-frontend/src/test/java/org/sonar/go/utils/ConstantResolutionTest.java
+++ sonar-go-frontend/src/test/java/org/sonar/go/utils/ConstantResolutionTest.java
@@ -19,6 +19,7 @@ package org.sonar.go.utils;
 import java.util.Collections;
 import org.junit.jupiter.api.Test;
 import org.sonar.go.api.BinaryExpressionTree;
+import org.sonar.go.api.IdentifierTree;
 import org.sonar.go.api.NativeTree;
 import org.sonar.go.api.Token;
 import org.sonar.go.api.Tree;
@@ -27,6 +28,9 @@ import org.sonar.go.impl.StringLiteralTreeImpl;
 import org.sonar.go.impl.TextRangeImpl;
 import org.sonar.go.impl.TokenImpl;
 import org.sonar.go.persistence.conversion.StringNativeKind;
+import org.sonar.go.symbols.Scope;
+import org.sonar.go.symbols.Symbol;
+import org.sonar.go.symbols.Usage;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.sonar.go.api.BinaryExpressionTree.Operator.PLUS;
@@ -81,5 +85,89 @@ class ConstantResolutionTest {
     NativeTree tree = TreeCreationUtils.simpleNative(new StringNativeKind("Kind"), Collections.emptyList());
     BinaryExpressionTree binary = TreeCreationUtils.binary(PLUS, HELLO, tree);
     assertThat(ConstantResolution.resolveAsStringConstant(binary)).isEqualTo("Hello" + PLACEHOLDER);
+  }
+
+  @Test
+  void effectivelyFinalIdentifierConstantResolution() {
+    IdentifierTree id = TreeCreationUtils.identifier("ID");
+    Symbol symbol = new Symbol("type", Scope.FUNCTION);
+    symbol.getUsages().add(new Usage(id, HELLO, Usage.UsageType.DECLARATION));
+    id.setSymbol(symbol);
+    assertThat(ConstantResolution.resolveAsStringConstant(id)).isEqualTo("Hello");
+  }
+
+  @Test
+  void reAssignedIdentifierConstantResolution() {
+    IdentifierTree id = TreeCreationUtils.identifier("ID");
+    Symbol symbol = new Symbol("type", Scope.FUNCTION);
+    symbol.getUsages().add(new Usage(id, HELLO, Usage.UsageType.DECLARATION));
+    symbol.getUsages().add(new Usage(id, WORLD, Usage.UsageType.ASSIGNMENT));
+    id.setSymbol(symbol);
+    assertThat(ConstantResolution.resolveAsStringConstant(id)).isEqualTo(PLACEHOLDER);
+  }
+
+  @Test
+  void noDeclarationIdentifierConstantResolution() {
+    IdentifierTree id = TreeCreationUtils.identifier("ID");
+    Symbol symbol = new Symbol("type", Scope.FUNCTION);
+    symbol.getUsages().add(new Usage(id, WORLD, Usage.UsageType.ASSIGNMENT));
+    id.setSymbol(symbol);
+    assertThat(ConstantResolution.resolveAsStringConstant(id)).isEqualTo(PLACEHOLDER);
+  }
+
+  @Test
+  void noValueInDeclarationConstantResolution() {
+    IdentifierTree id = TreeCreationUtils.identifier("ID");
+    Symbol symbol = new Symbol("type", Scope.FUNCTION);
+    symbol.getUsages().add(new Usage(id, null, Usage.UsageType.DECLARATION));
+    id.setSymbol(symbol);
+    assertThat(ConstantResolution.resolveAsStringConstant(id)).isEqualTo(PLACEHOLDER);
+  }
+
+  @Test
+  void multipleDeclarationsIdentifierConstantResolution() {
+    IdentifierTree id = TreeCreationUtils.identifier("ID");
+    Symbol symbol = new Symbol("type", Scope.FUNCTION);
+    symbol.getUsages().add(new Usage(id, HELLO, Usage.UsageType.DECLARATION));
+    symbol.getUsages().add(new Usage(id, WORLD, Usage.UsageType.DECLARATION));
+    id.setSymbol(symbol);
+    assertThat(ConstantResolution.resolveAsStringConstant(id)).isEqualTo(PLACEHOLDER);
+  }
+
+  @Test
+  void identifierWithNoSymbolConstantResolution() {
+    IdentifierTree id = TreeCreationUtils.identifier("ID");
+    assertThat(ConstantResolution.resolveAsStringConstant(id)).isEqualTo(PLACEHOLDER);
+  }
+
+  @Test
+  void nullConstantResolution() {
+    assertThat(ConstantResolution.resolveAsStringConstant(null)).isEqualTo(PLACEHOLDER);
+  }
+
+  @Test
+  void aliasIdentifierConstantResolution() {
+    IdentifierTree id = TreeCreationUtils.identifier("ID");
+    Symbol symbol = new Symbol("type", Scope.FUNCTION);
+    symbol.getUsages().add(new Usage(id, HELLO, Usage.UsageType.DECLARATION));
+    id.setSymbol(symbol);
+
+    IdentifierTree idAlias = TreeCreationUtils.identifier("IDAlias");
+    Symbol symbolAlias = new Symbol("type", Scope.FUNCTION);
+    symbolAlias.getUsages().add(new Usage(idAlias, id, Usage.UsageType.DECLARATION));
+    idAlias.setSymbol(symbolAlias);
+
+    assertThat(ConstantResolution.resolveAsStringConstant(idAlias)).isEqualTo(PLACEHOLDER);
+  }
+
+  @Test
+  void binaryInDeclarationConstantConstantResolution() {
+    IdentifierTree id = TreeCreationUtils.identifier("ID");
+    Symbol symbol = new Symbol("type", Scope.FUNCTION);
+    BinaryExpressionTree binary = TreeCreationUtils.binary(PLUS, HELLO, WORLD);
+    symbol.getUsages().add(new Usage(id, binary, Usage.UsageType.DECLARATION));
+    id.setSymbol(symbol);
+
+    assertThat(ConstantResolution.resolveAsStringConstant(id)).isEqualTo("HelloWorld");
   }
 }
